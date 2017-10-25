45e90a832779664545c2d868a6ccbe7228f94be8
Codruț Constantin Gușoi <codrut.gusoi@gmail.com>
2017-10-26 00:18:11 +0300

    Adds automatic test for submitted files

--- ci/check-sources.sh
+++ ci/check-sources.sh
@@ -1,6 +1,6 @@
 for d in ./src/*/ ; do
-  ruby   $d"helloworld.rb"
-  python $d"helloworld.py"
-  sh     $d"helloworld.sh"
-  node   $d"helloworld.js"
+  [ "$(ruby   $d'helloworld.rb')" == "Hello World" ] || (echo 'Error inside '$d'helloworld.rb') && exit 1
+  [ "$(python $d'helloworld.py')" == "Hello World" ] || (echo 'Error inside '$d'helloworld.py') && exit 1
+  [ "$(sh     $d'helloworld.sh')" == "Hello World" ] || (echo 'Error inside '$d'helloworld.sh') && exit 1
+  [ "$(node   $d'helloworld.js')" == "Hello World" ] || (echo 'Error inside '$d'helloworld.js') && exit 1
 done
