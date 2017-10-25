0bdcc8ae8213b2741e872a40a6540c93ef37be57
Codruț Constantin Gușoi <codrut.gusoi@gmail.com>
2017-10-26 00:23:06 +0300

    More parenthesis

--- ci/check-sources.sh
+++ ci/check-sources.sh
@@ -1,6 +1,6 @@
 for d in ./src/*/ ; do
-  [ "$(ruby   $d'helloworld.rb')" == "Hello World" ] || (echo 'Error inside '$d'helloworld.rb') && exit 1
-  [ "$(python $d'helloworld.py')" == "Hello World" ] || (echo 'Error inside '$d'helloworld.py') && exit 1
-  [ "$(sh     $d'helloworld.sh')" == "Hello World" ] || (echo 'Error inside '$d'helloworld.sh') && exit 1
-  [ "$(node   $d'helloworld.js')" == "Hello World" ] || (echo 'Error inside '$d'helloworld.js') && exit 1
+  [ "$(ruby   $d'helloworld.rb')" == "Hello World" ] || ((echo 'Error inside '$d'helloworld.rb') && exit 1)
+  [ "$(python $d'helloworld.py')" == "Hello World" ] || ((echo 'Error inside '$d'helloworld.py') && exit 1)
+  [ "$(sh     $d'helloworld.sh')" == "Hello World" ] || ((echo 'Error inside '$d'helloworld.sh') && exit 1)
+  [ "$(node   $d'helloworld.js')" == "Hello World" ] || ((echo 'Error inside '$d'helloworld.js') && exit 1)
 done
